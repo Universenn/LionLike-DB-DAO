d6ec307af815c4dd69e0ee8db616aa0640e4be33
Win10ft\UserK <dnwndhks98@gmail.com>
2022-10-19 14:19:52 +0900

    Spring 추가

--- src/main/java/com/likelion/dao/UserDaoFactory.java
+++ src/main/java/com/likelion/dao/UserDaoFactory.java
@@ -1,12 +1,20 @@
 package com.likelion.dao;
 
+import org.springframework.beans.factory.annotation.Configurable;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration // Spring
 public class UserDaoFactory {
     // 조립을 해줍니다.
+    @Bean
     public UserDao awsUserDao(){
         AWSConnectionMaker awsConnectionMaker = new AWSConnectionMaker();
         UserDao userDao = new UserDao();
         return userDao;
     }
+
+    @Bean
     public UserDao localUserDao(){
         LoclaConnectionMaker loclaConnectionMaker = new LoclaConnectionMaker();
         UserDao userDao = new UserDao();
--- src/test/java/com/likelion/dao/UserDaoTest.java
+++ src/test/java/com/likelion/dao/UserDaoTest.java
@@ -3,20 +3,40 @@ package com.likelion.dao;
 import com.likelion.domain.User;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.ApplicationContext;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
 
+@ExtendWith(SpringExtension.class)
+@ContextConfiguration(classes = UserDaoFactory.class)
 class UserDaoTest {
 
+    @Autowired //Spring
+    ApplicationContext context;
     @Test
     void addAndSelect() {
 //        UserDao userDao = new UserDao(new LoclaConnectionMaker());
-        UserDao userDao = new UserDaoFactory().awsUserDao();
+        UserDao userDao = context.getBean("awsUserDao", UserDao.class);
+        UserDao userDao2 = context.getBean("awsUserDao", UserDao.class);
 
-        String id = "7";
-        User user = new User(id,"plz", "110055");
+
+        // 주소가 같다 (Single Tone(ApplicationContext)을 써야하는 이유
+        // 중요하다..왜지?
+        //com.likelion.dao.UserDao@7fab4be7
+        //com.likelion.dao.UserDao@7fab4be7
+        System.out.println(userDao);
+        System.out.println(userDao2);
+
+
+
+
+        String id = "8";
+        User user = new User(id,"juwan", "110055qwe");
         userDao.add(user);
 
-        User selectedUser = userDao.findById(id);
-        Assertions.assertEquals("plz",selectedUser.getName());
-        Assertions.assertEquals("110055",selectedUser.getPassword());
+        User userSelecte = userDao.findById(id);
+        Assertions.assertEquals("juwan",userSelecte.getName());
     }
 }
